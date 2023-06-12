ef4e3b5fcb761b65b84f864e8c7a23fddc28c466
Leonardo Rodenas Escobar <83415965+Leonardo-Rodenas@users.noreply.github.com>
2023-06-12 17:16:50 -0400

    Delete telovendo directory

--- telovendo/telovendoapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import lista_usuarios
-
-urlpatterns = [
-    path('', views.mensaje, name='telovendo'),
-    path('usuarios/', lista_usuarios, name='lista_usuarios'),
-]
-
